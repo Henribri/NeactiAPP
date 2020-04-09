c906d1df74f85a0b8340f876619a5c092de86e38
Henribri <henri.briaux@viacesi.fr>
2020-04-09 22:31:41 +0200

    Invite 2.0

--- lib/invite.dart
+++ lib/invite.dart
@@ -1,11 +1,21 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'composer.dart';
 
 
 
-class Invite extends StatelessWidget {
+class Invite extends StatefulWidget {
+  @override
+  _InviteState createState() => _InviteState();
+}
+
+class _InviteState extends State<Invite> {
   final _formKey = GlobalKey<FormState>();
+  String _date = "Non définie";
+  String _time = "Non définie";
+
+
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
@@ -42,6 +52,7 @@ class Invite extends StatelessWidget {
               margin: EdgeInsets.all(10),
               child: TextFormField(
                   cursorColor: Colors.red,
+
 
                   decoration: InputDecoration(
                       labelStyle: TextStyle(color: Colors.black),
@@ -86,6 +97,54 @@ class Invite extends StatelessWidget {
                   }
               ),
             ),
+            
+            
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+
+                FlatButton.icon(
+                  onPressed: (){showDatePicker(context: context, initialDate: DateTime.now(), firstDate: DateTime(2020), lastDate: DateTime(3000)).then((date){
+                    setState(() {
+                      _date= DateFormat("dd-MM-yyyy").format(date).toString();
+                    });
+                  });},
+                  icon: Icon(Icons.date_range),
+                  label: Text(_date),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: new BorderRadius.circular(5.0),
+                    side: BorderSide(color: Colors.red)
+                  ),
+                ),
+
+                FlatButton.icon(
+                onPressed: (){showTimePicker(context: context, initialTime: TimeOfDay.now()).then((time){
+
+                  setState(() {
+                    _time='${time.hour} : ${time.minute}';
+
+                  });
+                });
+                    },
+                  icon: Icon(Icons.access_time),
+                  label: Text(_time),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: new BorderRadius.circular(5.0),
+                      side: BorderSide(color: Colors.red)
+                  ),
+                ),
+
+              ],
+            ),
+            
+            
+            
+            
+            
+            
+            
+            
+            
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: <Widget>[
@@ -116,34 +175,12 @@ class Invite extends StatelessWidget {
                     }
                 ),
               ),
-                Container(
-                  width: 130,
-                  margin: EdgeInsets.all(10),
-                  child: TextFormField(
-                      cursorColor: Colors.red,
 
-                      decoration: InputDecoration(
-                        prefixIcon: Icon(Icons.date_range),
-                          labelStyle: TextStyle(color: Colors.black, fontSize: 14),
-                          labelText: 'Date',
-                          focusedBorder: OutlineInputBorder(
-                              borderSide: BorderSide(color: Colors.red)
-                          ),
-                          enabledBorder: OutlineInputBorder(
-                              borderSide: BorderSide(color: Colors.red)
-                          )
-                      ),
-                      validator: (value){
-                        if(value.isEmpty){
-                          return "Please enter some text";
-                        }
-                        return null;
-                      }
-                  ),
-                ),
+
               ]
               ,
             ),
+      
       RaisedButton(
       onPressed: () {
       // Validate returns true if the form is valid, otherwise false.
