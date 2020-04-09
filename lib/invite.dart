92173550980448f3c436dfa36788717f33a97382
Henribri <henri.briaux@viacesi.fr>
2020-04-09 15:24:29 +0200

    new bar

--- lib/invite.dart
+++ lib/invite.dart
@@ -1,16 +1,168 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'composer.dart';
 
 
+
 class Invite extends StatelessWidget {
+  final _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
+    return SingleChildScrollView(
+      child: Form(
+        key: _formKey,
+        child: Column(
+          children: <Widget>[
+            Container(
+              width: 340,
+              margin: EdgeInsets.all(10),
+              child: TextFormField(
+                cursorColor: Colors.red,
+
+                decoration: InputDecoration(
+                  labelStyle: TextStyle(color: Colors.black),
+                  labelText: 'Titre',
+                  focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.red)
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.red)
+                  )
+                ),
+                validator: (value){
+                  if(value.isEmpty){
+                    return "Please enter some text";
+                  }
+                  return null;
+                }
+              ),
+            ),
+            Container(
+              width: 340,
+              margin: EdgeInsets.all(10),
+              child: TextFormField(
+                  cursorColor: Colors.red,
+
+                  decoration: InputDecoration(
+                      labelStyle: TextStyle(color: Colors.black),
+                      labelText: 'Message',
+                      focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.red)
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.red)
+                      )
+                  ),
+                  validator: (value){
+                    if(value.isEmpty){
+                      return "Please enter some text";
+                    }
+                    return null;
+                  }
+              ),
+            ),
+            Container(
+              width: 340,
+              margin: EdgeInsets.all(10),
+              child: TextFormField(
+                  cursorColor: Colors.red,
+
+                  decoration: InputDecoration(
+                      labelStyle: TextStyle(color: Colors.black),
+                      labelText: 'Lieu',
+                      prefixIcon: Icon(Icons.location_on, color: Colors.red,),
+                      focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.red)
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.red)
+                      )
+                  ),
+                  validator: (value){
+                    if(value.isEmpty){
+                      return "Please enter some text";
+                    }
+                    return null;
+                  }
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+              Container(
+                width: 130,
+                margin: EdgeInsets.all(10),
+                child: TextFormField(
+
+                    cursorColor: Colors.red,
+
+                    decoration: InputDecoration(
+                      prefixIcon: Icon(Icons.group_add),
+
+                        labelStyle: TextStyle(color: Colors.black, fontSize: 14),
+                        labelText: 'Nombre',
+                        focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.red)
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.red)
+                        )
+                    ),
+                    validator: (value){
+                      if(value.isEmpty){
+                        return "Please enter some text";
+                      }
+                      return null;
+                    }
+                ),
+              ),
+                Container(
+                  width: 130,
+                  margin: EdgeInsets.all(10),
+                  child: TextFormField(
+                      cursorColor: Colors.red,
+
+                      decoration: InputDecoration(
+                        prefixIcon: Icon(Icons.date_range),
+                          labelStyle: TextStyle(color: Colors.black, fontSize: 14),
+                          labelText: 'Date',
+                          focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.red)
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.red)
+                          )
+                      ),
+                      validator: (value){
+                        if(value.isEmpty){
+                          return "Please enter some text";
+                        }
+                        return null;
+                      }
+                  ),
+                ),
+              ]
+              ,
+            ),
+      RaisedButton(
+      onPressed: () {
+      // Validate returns true if the form is valid, otherwise false.
+      if (_formKey.currentState.validate()) {
+      // If the form is valid, display a snackbar. In the real world,
+      // you'd often call a server or save the information in a database.
+
+      Scaffold
+          .of(context)
+          .showSnackBar(SnackBar(content: Text('Processing Data')));
+      }
+      },
+      child: Text('Submit'),
+      ),
 
 
-
-      ],
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/plans.dart
+++ lib/plans.dart
@@ -9,239 +9,230 @@ class _PlansState extends State<Plans> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color.fromARGB(210, 255, 0, 60),
+        title: Text('Mes plans',
+          style: TextStyle(
+              color: Colors.white,
+              fontFamily: 'Fred',
+              fontSize: 26
+          ),),
+      ),
 
       backgroundColor: Colors.blueGrey[50],
-      body: CustomScrollView(
-        slivers: <Widget>[
-          const SliverAppBar(
-            backgroundColor: Color.fromARGB(210, 255, 0, 60),
-            pinned: true,
-            expandedHeight: 50,
-            flexibleSpace: FlexibleSpaceBar(
-              title: Text('Mes plans',
-                style: TextStyle(
-                    color: Colors.white,
-                  fontFamily: 'Fred',
-                  fontSize: 26
-                ),),
-            ),
-      actions: <Widget>[
-        IconButton(
-            icon: const Icon(Icons.search, size: 40, color: Colors.white,),
-            tooltip: 'Add new entry',
+      body: SingleChildScrollView(
 
-    ),]
-          ),
-          SliverList(
-            delegate: SliverChildListDelegate(
-                [
-                  SizedBox(height: 10,),
-                  Card(
-                    child: Column(
-                      children: <Widget>[
-                        ExpansionTile(
-                          leading: Icon(
-                            Icons.videogame_asset, size: 40, color: Colors.red,),
-                          title: Text(
-                            'Tournoi Smash',
-                            style: TextStyle(
-                                fontSize: 20,
-                                fontWeight: FontWeight.bold
-                            ),),
-                          subtitle: Text('Ramenez vos manettes !',
-                            style: TextStyle(
-                                fontSize: 16
-                            ),),
+        child: Column(
+          children: <Widget>[
+            SizedBox(height: 10,),
+            Card(
+              child: Column(
+                children: <Widget>[
+                  ExpansionTile(
+                    leading: Icon(
+                      Icons.videogame_asset, size: 40, color: Colors.red,),
+                    title: Text(
+                      'Tournoi Smash',
+                      style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold
+                      ),),
+                    subtitle: Text('Ramenez vos manettes !',
+                      style: TextStyle(
+                          fontSize: 16
+                      ),),
 
+                    children: <Widget>[
+
+                      Padding(
+                        padding: EdgeInsets.fromLTRB(15, 10, 0, 5),
+                        child: Row(
                           children: <Widget>[
-
-                            Padding(
-                              padding: EdgeInsets.fromLTRB(15, 10, 0, 5),
-                              child: Row(
-                                children: <Widget>[
-                                  Icon(Icons.date_range, size: 30,),
-                                  SizedBox(width: 20,),
-                                  Text('12-05-2020',
-                                    style: TextStyle(
-                                        fontSize: 18
-                                    ),),
-                                ],
-                              ),
-                            ),
-                            SizedBox(height: 10,),
-                            Padding(
-                              padding: EdgeInsets.fromLTRB(15, 0, 0, 5),
-                              child: Row(
-                                children: <Widget>[
-                                  Icon(Icons.location_on, size: 30,),
-                                  SizedBox(width: 20,),
-                                  Text('7 rue du marais',
-                                    style: TextStyle(
-                                      fontSize: 18,
-
-                                    ),),
-                                ],
-                              ),
-                            ),
-                            SizedBox(height: 10,),
-                            Padding(
-                              padding: EdgeInsets.fromLTRB(15, 0, 0, 5),
-                              child: Row(
-                                children: <Widget>[
-                                  Icon(Icons.group, size: 30,),
-                                  SizedBox(width: 20,),
-                                  Text('10/14',
-                                    style: TextStyle(
-                                        fontSize: 18
-                                    ),),
-                                ],
-                              ),
-                            ),
-                            SizedBox(height: 10),
-                            Padding(
-                              padding: EdgeInsets.fromLTRB(15, 0, 0, 5),
-                              child: Row(
-                                children: <Widget>[
-                                  Icon(Icons.description, size: 30,),
-                                  SizedBox(width: 20,),
-                                  Expanded(
-                                    child: Container(
-                                      child: Text('Description',
-                                        maxLines: 3,
-                                        style: TextStyle(
-                                            fontSize: 18
-                                        ),),
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ),
-                            ButtonBar(
-                                children: <Widget>[
-                                  FlatButton(
-                                    color: Colors.red,
-                                    child: Text('Join'),
-                                    onPressed: () {
-
-                                    },
-                                  )
-                                ]),
+                            Icon(Icons.date_range, size: 30,),
+                            SizedBox(width: 20,),
+                            Text('12-05-2020',
+                              style: TextStyle(
+                                  fontSize: 18
+                              ),),
                           ],
                         ),
+                      ),
+                      SizedBox(height: 10,),
+                      Padding(
+                        padding: EdgeInsets.fromLTRB(15, 0, 0, 5),
+                        child: Row(
+                          children: <Widget>[
+                            Icon(Icons.location_on, size: 30,),
+                            SizedBox(width: 20,),
+                            Text('7 rue du marais',
+                              style: TextStyle(
+                                fontSize: 18,
 
+                              ),),
+                          ],
+                        ),
+                      ),
+                      SizedBox(height: 10,),
+                      Padding(
+                        padding: EdgeInsets.fromLTRB(15, 0, 0, 5),
+                        child: Row(
+                          children: <Widget>[
+                            Icon(Icons.group, size: 30,),
+                            SizedBox(width: 20,),
+                            Text('10/14',
+                              style: TextStyle(
+                                  fontSize: 18
+                              ),),
+                          ],
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      Padding(
+                        padding: EdgeInsets.fromLTRB(15, 0, 0, 5),
+                        child: Row(
+                          children: <Widget>[
+                            Icon(Icons.description, size: 30,),
+                            SizedBox(width: 20,),
+                            Expanded(
+                              child: Container(
+                                child: Text('Description',
+                                  maxLines: 3,
+                                  style: TextStyle(
+                                      fontSize: 18
+                                  ),),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      ButtonBar(
+                          children: <Widget>[
+                            FlatButton(
+                              color: Colors.red,
+                              child: Text('Leave'),
+                              onPressed: () {
 
-                      ],
-                    ),
+                              },
+                            )
+                          ]),
+                    ],
                   ),
 
 
-                  SizedBox(height: 10,),
-                  Card(
-                    child: Column(
-                      children: <Widget>[
-                        ExpansionTile(
-                          leading: Icon(
-                            Icons.videogame_asset, size: 40, color: Colors.red,),
-                          title: Text(
-                            'Tournoi Smash',
-                            style: TextStyle(
-                                fontSize: 20,
-                                fontWeight: FontWeight.bold
-                            ),),
-                          subtitle: Text('Ramenez vos manettes !',
-                            style: TextStyle(
-                                fontSize: 16
-                            ),),
+                ],
+              ),
+            ),
 
+
+            SizedBox(height: 10,),
+            Card(
+              child: Column(
+                children: <Widget>[
+                  ExpansionTile(
+                    leading: Icon(
+                      Icons.videogame_asset, size: 40, color: Colors.red,),
+                    title: Text(
+                      'Tournoi Smash',
+                      style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold
+                      ),),
+                    subtitle: Text('Ramenez vos manettes !',
+                      style: TextStyle(
+                          fontSize: 16
+                      ),),
+
+                    children: <Widget>[
+
+                      Padding(
+                        padding: EdgeInsets.fromLTRB(15, 10, 0, 5),
+                        child: Row(
                           children: <Widget>[
-
-                            Padding(
-                              padding: EdgeInsets.fromLTRB(15, 10, 0, 5),
-                              child: Row(
-                                children: <Widget>[
-                                  Icon(Icons.date_range, size: 30,),
-                                  SizedBox(width: 20,),
-                                  Text('12-05-2020',
-                                    style: TextStyle(
-                                        fontSize: 18
-                                    ),),
-                                ],
-                              ),
-                            ),
-                            SizedBox(height: 10,),
-                            Padding(
-                              padding: EdgeInsets.fromLTRB(15, 0, 0, 5),
-                              child: Row(
-                                children: <Widget>[
-                                  Icon(Icons.location_on, size: 30,),
-                                  SizedBox(width: 20,),
-                                  Text('7 rue du marais',
-                                    style: TextStyle(
-                                      fontSize: 18,
-
-                                    ),),
-                                ],
-                              ),
-                            ),
-                            SizedBox(height: 10,),
-                            Padding(
-                              padding: EdgeInsets.fromLTRB(15, 0, 0, 5),
-                              child: Row(
-                                children: <Widget>[
-                                  Icon(Icons.group, size: 30,),
-                                  SizedBox(width: 20,),
-                                  Text('10/14',
-                                    style: TextStyle(
-                                        fontSize: 18
-                                    ),),
-                                ],
-                              ),
-                            ),
-                            SizedBox(height: 10),
-                            Padding(
-                              padding: EdgeInsets.fromLTRB(15, 0, 0, 5),
-                              child: Row(
-                                children: <Widget>[
-                                  Icon(Icons.description, size: 30,),
-                                  SizedBox(width: 20,),
-                                  Expanded(
-                                    child: Container(
-                                      child: Text('Description',
-                                        maxLines: 3,
-                                        style: TextStyle(
-                                            fontSize: 18
-                                        ),),
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ),
-                            ButtonBar(
-                                children: <Widget>[
-                                  FlatButton(
-                                    color: Colors.red,
-                                    child: Text('Join'),
-                                    onPressed: () {
-
-                                    },
-                                  ),
-
-
-                                ]),
-
+                            Icon(Icons.date_range, size: 30,),
+                            SizedBox(width: 20,),
+                            Text('12-05-2020',
+                              style: TextStyle(
+                                  fontSize: 18
+                              ),),
                           ],
                         ),
+                      ),
+                      SizedBox(height: 10,),
+                      Padding(
+                        padding: EdgeInsets.fromLTRB(15, 0, 0, 5),
+                        child: Row(
+                          children: <Widget>[
+                            Icon(Icons.location_on, size: 30,),
+                            SizedBox(width: 20,),
+                            Text('7 rue du marais',
+                              style: TextStyle(
+                                fontSize: 18,
+
+                              ),),
+                          ],
+                        ),
+                      ),
+                      SizedBox(height: 10,),
+                      Padding(
+                        padding: EdgeInsets.fromLTRB(15, 0, 0, 5),
+                        child: Row(
+                          children: <Widget>[
+                            Icon(Icons.group, size: 30,),
+                            SizedBox(width: 20,),
+                            Text('10/14',
+                              style: TextStyle(
+                                  fontSize: 18
+                              ),),
+                          ],
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      Padding(
+                        padding: EdgeInsets.fromLTRB(15, 0, 0, 5),
+                        child: Row(
+                          children: <Widget>[
+                            Icon(Icons.description, size: 30,),
+                            SizedBox(width: 20,),
+                            Expanded(
+                              child: Container(
+                                child: Text('Description',
+                                  maxLines: 3,
+                                  style: TextStyle(
+                                      fontSize: 18
+                                  ),),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      ButtonBar(
+                          children: <Widget>[
+                            FlatButton(
+                              color: Colors.red,
+                              child: Text('Leave'),
+                              onPressed: () {
+
+                              },
+                            ),
 
 
-                      ],
-                    ),
+                          ]),
+
+                    ],
                   ),
-                ]
+
+
+                ],
+              ),
             ),
+          ],
+        ),
+
           ),
 
-        ],
-      )
+
+
+
 
 
 
